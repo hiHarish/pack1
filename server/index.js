1d22fd7f641fb11751efe6892bc6f95d775db5c6
hiHarish <156064772+hiHarish@users.noreply.github.com>
2025-07-26 22:03:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.post("/send-appointment", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;  ;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
